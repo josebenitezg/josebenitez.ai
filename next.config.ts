8b776ba8ccef5ca0ce8ec2da80b17049dd84366f
Jose Benitez <jose@intuitivo.com>
2025-03-27 19:41:26 -0700

    add new post

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,32 @@ const nextConfig: NextConfig = {
         hostname: 'images-na.ssl-images-amazon.com',
         pathname: '/images/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.tensorflow.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'demzamss7t8rinle.public.blob.vercel-storage.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'preview.redd.it',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.openart.ai',
+        pathname: '/**',
+      },
+      
     ],
   },
 };
